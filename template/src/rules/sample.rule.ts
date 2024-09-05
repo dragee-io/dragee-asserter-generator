7c9694a098e49ec1ce87ad5ca6ce8e1a845f2575
mbernasinski <contact@mbernasinski.com>
2024-09-05 17:24:34 +0200

    feat: adding rule severity
    https://github.com/MichaelBernasinski/dragee-asserter-model/commit/be811ec0162544a10ecee8e7ca6e0612266ee712

--- template/src/rules/sample.rule.ts
+++ template/src/rules/sample.rule.ts
@@ -1,8 +1,11 @@
-import type { Dragee, RuleResult } from '@dragee-io/asserter-type';
+import { type Dragee, type RuleResult, RuleSeverity } from '@dragee-io/asserter-type';
 import { TemplateRule } from '../template-rule.model.ts';
 
-export default new TemplateRule('Sample rule', (dragees: Dragee[]): RuleResult[] =>
-    dragees.map(() => {
-        return { pass: false, message: 'To be implemented' };
-    })
+export default new TemplateRule(
+    'Sample rule',
+    RuleSeverity.ERROR,
+    (dragees: Dragee[]): RuleResult[] =>
+        dragees.map(() => {
+            return { pass: false, message: 'To be implemented' };
+        })
 );
