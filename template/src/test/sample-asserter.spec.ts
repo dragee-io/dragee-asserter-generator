5c9c484cce96ed614794e07e5903ec34d5a92334
mbernasinski <contact@mbernasinski.com>
2024-10-14 16:14:53 +0200

    build: Biome config extending from dragee-model

--- template/src/test/sample-asserter.spec.ts
+++ template/src/test/sample-asserter.spec.ts
@@ -1,7 +1,7 @@
-import {describe, expect, test} from "bun:test";
-import { type Dependency, DependencyType } from "@dragee-io/type/common";
-import { type Report, asserterHandler } from "@dragee-io/type/asserter";
-import templateAsserter from "../..";
+import { describe, expect, test } from 'bun:test';
+import { type Report, asserterHandler } from '@dragee-io/type/asserter';
+import { type Dependency, DependencyType } from '@dragee-io/type/common';
+import templateAsserter from '../..';
 
 describe('Sample Asserter', () => {
     test('assert with no dragees', () => {
@@ -9,33 +9,37 @@ describe('Sample Asserter', () => {
         expect(report.pass).toBeTrue();
         expect(report.namespace).toBe('template');
     });
-    
+
     test('assert with dragees', () => {
         const dependancy: Dependency = {
-            'dragee1': DependencyType.FIELD
+            dragee1: DependencyType.FIELD
         };
-        const report: Report = asserterHandler(templateAsserter, [{
-            name: 'dragee1',
-            profile: 'template/test',
-            depends_on: []
-        },{
-            name: 'dragee2',
-            profile: 'template/test',
-            depends_on: [dependancy]
-        }]);
+        const report: Report = asserterHandler(templateAsserter, [
+            {
+                name: 'dragee1',
+                profile: 'template/test',
+                depends_on: []
+            },
+            {
+                name: 'dragee2',
+                profile: 'template/test',
+                depends_on: [dependancy]
+            }
+        ]);
 
         expect(report.pass).toBeFalse();
-        expect(report.errors).toEqual([            
+        expect(report.errors).toEqual([
             {
                 drageeName: 'dragee1',
                 message: 'To be implemented',
-                ruleId: "template/sample-rule"
-            },{
+                ruleId: 'template/sample-rule'
+            },
+            {
                 drageeName: 'dragee2',
                 message: 'To be implemented',
-                ruleId: "template/sample-rule"
-            }]
-        );
+                ruleId: 'template/sample-rule'
+            }
+        ]);
         expect(report.namespace).toBe('template');
     });
-})
+});
