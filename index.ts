6b34dafbb9544dccd05d617e68dbc3d6d5ecac29
mbernasinski <mbernasinski@hoppr.tech>
2024-10-18 15:32:06 +0200

    docs: Adding CLI command description and summary

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import { generatorHandler } from './src/command.handler.ts';
 
 export const generateAsserter = new Command('generate-asserter')
     .alias('ga')
-    .requiredOption('-n, --name <string>', 'name of the asserter project')
-    .requiredOption('-od, --output-dir <string>', 'output dir for the asserter project')
+    .summary('generates a new asserter project')
+    .description(
+        'Generates a new asserter project.\nBased on a standard dragee asserter template, with mandatory dependancies and a sample rule.'
+    )
+    .requiredOption('-n, --name <string>', 'name of the new asserter project')
+    .requiredOption('-od, --output-dir <string>', 'output dir for the new asserter project')
     .action(generatorHandler);
